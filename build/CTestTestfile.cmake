c0b2e41bfa69dfb5abcd38875220ab45541a0ca9
Ahad Rauf <ahadrauf@gmail.com>
2018-11-01 15:31:38 -0700

    initializing grasping package

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/cc/ee106a/fa18/class/ee106a-aab/ros_workspaces/TheCreator/src
+# Build directory: /home/cc/ee106a/fa18/class/ee106a-aab/ros_workspaces/TheCreator/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
+SUBDIRS(grasping)
---
+++ build/grasping/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/cc/ee106a/fa18/class/ee106a-aab/ros_workspaces/TheCreator/src/grasping
+# Build directory: /home/cc/ee106a/fa18/class/ee106a-aab/ros_workspaces/TheCreator/build/grasping
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
